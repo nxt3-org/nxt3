6483df6338c35144c2b3bc694de9f447affce06d
Jakub Vaněk <vanek.jakub4@seznam.cz>
2020-08-16 19:54:39 +0200

    Fix forgotten RBF file of SFX installer

--- firmware/installer/sfx.sh
+++ firmware/installer/sfx.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-tail -c +154 "$0" | tar -C "/home/root/lms2012/prjs" -xzf -
-rm -f "$0" "$(basename "$0" .sh).rbf"
+tail -c +155 "$0" | tar -C "/home/root/lms2012/prjs" -xzf -
+rm -f "$0" "$(basename "$0" .elf).rbf"
 exec "/home/root/lms2012/prjs/NXT3/nxt3.elf"
